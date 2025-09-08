e0feaaf04dbfb1904605aa8f424f988f9e64c974
MaBenAd <mbadjevi@gmail.com>
2025-09-08 20:12:13 +0100

    General update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,18 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+keymap.set("i", "jj", "<ESC>", { noremap = false })
+
 -- Last telescope
 keymap.set("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
+
+-- select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New Tabs
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
